153a8fd0772706ce65e36cfa964b721fe75b392d
Pacchu <37984032+itspacchu@users.noreply.github.com>
2021-06-28 13:59:13 +0530

    why is webhook acting so weird im gonna question my sanity

--- mainbot/bot_mixins/stickerhandler.py
+++ mainbot/bot_mixins/stickerhandler.py
@@ -12,7 +12,7 @@ class stickerHandler(DiscordInit, commands.Cog):
                 match = self.discordStickers.find_one(query)['stickerurl']
                 webhook = await ctx.channel.create_webhook("pacchu_webhook")
                 await webhook.send(match, username=ctx.author.name, avatar_url=ctx.author.avatar_url)
-                await ctx.channel.webhooks()
+                await webhook.delete()
                 await ctx.message.delete()
             except:
                 embed = discord.Embed(color=0xffffff, description=f"Supported Stickers (more adding soon)")
@@ -24,12 +24,12 @@ class stickerHandler(DiscordInit, commands.Cog):
             await ctx.channel.send(e)
 
     @commands.command(aliases=['impersonate','sayas'])
-    async def impersonator(self, ctx, whom:discord.Member,*text):
+    async def impersonator(self,ctx,whom:discord.Member,*text):
             text = queryToName(text)
             try:
                 webhook = await ctx.channel.create_webhook("pacchu_webhook")
-                await webhook.send(text, username=whom.name + '*' , avatar_url=whom.avatar_url)
-                await ctx.channel.webhooks()
+                await webhook.send(text, username=whom.display_name + '*', avatar_url=whom.avatar_url)
+                await webhook.delete()
                 await ctx.message.delete()
             except Exception as e:
                 await ctx.message.add_reaction('<:pacDoubleExclaim:858677949775872010>')

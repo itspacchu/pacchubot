b652e429fd4679347b5a8f8a3f78686157852462
itspacchu <37984032+itspacchu@users.noreply.github.com>
2021-10-07 15:09:12 +0530

    Pacchimari bot instance

--- mainbot/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bot_mixins import *
-from .core import *
-# from .__imports__ import *
-from .settings import *
-
--- mainbot/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import gpt2api as g2a
-from . import podcasthandler as ph
-from .injectPayload import cartoonize
-
-__all__ = [
-    'g2a',
-    'cartoonize',
-    'ph',
-]

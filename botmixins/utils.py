8777f2d1f3aa468dde4aa507a88be91fb174e17b
Prashant <37984032+itspacchu@users.noreply.github.com>
2023-10-05 17:24:50 +0530

    Dominant color is too much processing

--- botmixins/utils.py
+++ botmixins/utils.py
@@ -30,28 +30,3 @@ class Colors:
     APPLE = 0xfc3c44
     OTHER = 0xeba434
 
-    def dominant(imageurl: str, local=False):
-        try:
-            NUM_CLUSTERS = 10
-            if(local):
-                im = Image.open(imageurl)
-            else:
-                im = Image.open(requests.get(imageurl, stream=True).raw)
-            im = im.resize((25, 25))
-            ar = np.asarray(im)
-            shape = ar.shape
-            ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
-            codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-            vecs, dist = scipy.cluster.vq.vq(ar, codes)
-            counts, bins = scipy.histogram(vecs, len(codes))
-            index_max = scipy.argmax(counts)
-            peak = codes[index_max]
-            colour = binascii.hexlify(bytearray(int(c)
-                                    for c in peak)).decode('ascii')
-            try:
-                return int(hex(int(colour, 16))[:8], 0)
-            except:
-                return 0xffffff
-        except IndexError:
-            return 0xffffff
-
